0e225c7eda67e976d9da5e5d579f587966462076
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 13:05:15 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //make the scanner for input
+    Scanner input = new Scanner(System.in);
+    // ask for grade
+    System.out.println("Please enter a letter grade");
+    String grade = input.nextLine();
+    // determine the mark
+    switch(grade){
+      case "A":
+      System.out.println("This is between 80% and 100%");
+      break;
+      case "B":
+      System.out.println("This is between 70% and 79%");
+      break;
+      case "C":
+      System.out.println("This is between 60% and 69%");
+      break;
+      case "D":
+      System.out.println("This is between 50% and 59%");
+      break;
+      case "F":
+      System.out.println("This is between 0% and 49%");
+      break;
+    }
     
   }
 }
